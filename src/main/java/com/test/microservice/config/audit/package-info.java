9380bbfd7964334255f0c12864c0c86d9b33f251
Yogesh Patel <yogeshpatel276052@gmail.com>
2019-09-14 02:02:23 -0400

    Initial application generated by JHipster-6.2.0

---
+++ src/main/java/com/test/microservice/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.test.microservice.config.audit;
---
+++ src/main/java/com/test/microservice/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.test.microservice.repository;
+
+import com.test.microservice.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
